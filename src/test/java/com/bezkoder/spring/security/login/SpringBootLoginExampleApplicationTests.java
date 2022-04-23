e09742e802b894b460b7512fd9f9a5dec6d82e3e
mohanmarlapalli <30617999+mohanmarlapalli@users.noreply.github.com>
2022-04-23 14:04:58 +0530

    Update SpringBootLoginExampleApplicationTests.java

--- src/test/java/com/bezkoder/spring/security/login/SpringBootLoginExampleApplicationTests.java
+++ src/test/java/com/bezkoder/spring/security/login/SpringBootLoginExampleApplicationTests.java
@@ -1,13 +1,14 @@
-package com.bezkoder.spring.security.login;
+//package com.bezkoder.spring.security.login;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBootSecurityLoginApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBootLoginExampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
